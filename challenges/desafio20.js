49fd81069a1be2fe64bd9d01cec65c13a4ff89cd
Tamiris Shigaki <shigakiendo@gmail.com>
2022-09-09 13:53:56 -0300

    refactor: :recycle: Alteração do operador

--- challenges/desafio20.js
+++ challenges/desafio20.js
@@ -1,3 +1,3 @@
-db.produtos.updateMany({ nome: "Quarteirão com Queijo" }, { $pop: { ingredientes: -1 } });
+db.produtos.updateOne({ nome: "Quarteirão com Queijo" }, { $pop: { ingredientes: -1 } });
 
 db.produtos.find({}, { nome: 1, ingredientes: 1, _id: 0 }).pretty();
